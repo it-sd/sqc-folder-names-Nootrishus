a8b3128d4df3c6b7d3a6da84a3613040182ae50e
Michelle DelCastillo <michelle.delcastillo@student.cvtc.edu>
2023-02-15 19:29:12 -0600

    Adding JavaScript

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-const newFolderName = function (folders) {
+function newFolderName(folderNames) {
 
+    let largestIndex = 0;
+    for (let i = 0; i < folderNames.length; i++) {
+        const folderName = folderNames[i];
+        if (folderName.startsWith("New Folder")) {
+            const indexString =folderName.replace("NewFolder", "").trim();
+            if (!isNaN(indexString)) {
+                const index = parseInt(indexString);
+                largestIndex = Math.max(largestIndex, index);
+            }
+        }
+    }
+    if (largestIndex === 0) {
+        return "New Folder";
+    } else {
+        return "New Folder" + (largestIndex + 1);
+    }
 }
